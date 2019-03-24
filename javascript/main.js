9306a71a29a76a48c1e87203d5ed522358af3f32
CarlosGonzalezCruz <carlosgonzalezcruz98@gmail.com>
2019-03-24 22:19:53 +0100

    Interfaz

---
+++ javascript/main.js
@@ -0,0 +1,46 @@
+console.log("polla");
+console.log("grande");
+console.log("UwU");
+var State;
+(function (State) {
+    State[State["STATS"] = 0] = "STATS";
+    State[State["BREEDING"] = 1] = "BREEDING";
+    State[State["CATCHING"] = 2] = "CATCHING";
+    State[State["TYPE_COMPATIBILITY"] = 3] = "TYPE_COMPATIBILITY";
+    State[State["FORMS"] = 4] = "FORMS";
+})(State || (State = {}));
+var stateNames = ["Stats", "Breed", "Catch", "Typing", "Forms"];
+var state = State.STATS;
+var screenQueries;
+var $displayScreen;
+$(document).ready(function () {
+    screenQueries = [$("#stats"), $("#breeding"), $("#catching"), $("#typeCompatibility"), $("#forms")];
+    $displayScreen = $("#displayScreen");
+    updateScreensWithState(state);
+    $("#displayL").click(function () {
+        state = state == 0 ? 4 : (state - 1);
+        updateScreensWithState(state);
+    });
+    $("#displayR").click(function () {
+        state = state == 4 ? 0 : (state + 1);
+        updateScreensWithState(state);
+    });
+});
+function updateScreensWithState(state) {
+    if (!screenQueries || !$displayScreen)
+        return;
+    for (var i = 0; i < screenQueries.length; i++) {
+        var prevState = state == 0 ? (screenQueries.length - 1) : (state - 1);
+        if (i == state) {
+            screenQueries[i].css("transform", "translateX(0) scaleX(1)");
+        }
+        else if (i == prevState) {
+            screenQueries[i].css("transform", "translateX(-150px) scaleX(0)");
+        }
+        else {
+            screenQueries[i].css("transform", "translateX(180px) scaleX(0)");
+        }
+    }
+    $displayScreen.text(stateNames[state]);
+}
+//# sourceMappingURL=main.js.map
---
+++ typescript/definitions/jquery.d.ts
@@ -0,0 +1,38 @@
+declare function $(element :HTMLDocument | string) :jQuery;
+
+declare class jQuery {
+    /** Ejecutar cuando el documento esté listo */
+    ready(listener :() => void) :void;
+    /** Devuelve el hijo en la posición `index` dentro del elemento de la consulta, como elemento HTML */
+    get(index? :number) :HTMLElement | HTMLElement[] | undefined;
+    /** Devuelve una consulta referida al primer hijo */
+    first() :jQuery;
+    /** Devuelve una consulta referida al último hijo */
+    last() :jQuery;
+    /** Ejecutar al hacer click */
+    click(listener :() => void) :void;
+    /** Devuelve o modifica el contenido sin interpretarlo como HTML */
+    text(content? :string) :string;
+    /** Devuelve o modifica el contenido interpretándolo como HTML */
+    html(content? :string) :string;
+    /** Modifica un par clave-valor del atributo style */
+    css(key :string, value :string) :void;
+    /** Devuelve o modifica el valor de un elemento de formulario */
+    val(content? :string | number | boolean) :string | number | boolean;
+    /** Devuelve o modifica el valor del atributo especificado */
+    attr(attribute :string, value? :string) :string;
+    /** Añade una clase de CSS al elemento de la consulta */
+    addClass(cl :string) :void;
+    /** Elimina una clase de CSS al elemento de la consulta */
+    removeClass(cl :string) :void;
+    /** Alterna una clase de CSS al elemento de la consulta (la elimina si ya está, la añade si no) */
+    toggleClass(cl :string) :void;
+    /** Devuelve una nueva consulta referida a todos los hijos en profundidad del elemento de la consulta actual */
+    children() :jQuery;
+    /** Devuelve una nueva consulta referida a los hijos de primer nivel del elemento de la consulta actual */
+    find() :jQuery;
+    /** Elimina el atributo hidden */
+    show() :void;
+    /** Añade el atributo hidden */
+    hide() :void;
+}
